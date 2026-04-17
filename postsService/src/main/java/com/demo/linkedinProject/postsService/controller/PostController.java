04c2566380f5b69c1114b873a7308ae973e90e41
Anshika <dakshanshika1995@gmail.com>
2026-04-17 13:12:20 +0530

    config server client feature added

--- postsService/src/main/java/com/demo/linkedinProject/postsService/controller/PostController.java
+++ postsService/src/main/java/com/demo/linkedinProject/postsService/controller/PostController.java
@@ -1,11 +1,13 @@
 package com.demo.linkedinProject.postsService.controller;
 
+import com.demo.linkedinProject.postsService.contextHolder.AuthContextHolder;
 import com.demo.linkedinProject.postsService.dto.PostCreateRequestDto;
 import com.demo.linkedinProject.postsService.dto.PostDto;
 import com.demo.linkedinProject.postsService.service.PostService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +20,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostController {
 
+    @Value("${my.property}")
+    private String myproperty;
 
     private final PostService postService;
 
+    private final AuthContextHolder authContextHolder;
+
+//    @GetMapping("/test")
+//    public ResponseEntity<String> welcome(@RequestHeader("X-User-Id") String userId){
+//        return ResponseEntity.ok("Welcome : "+ userId);
+//    }
+
+//    @GetMapping("/test")
+//    public ResponseEntity<String> welcome(HttpServletRequest request){
+//        String userId = request.getHeader("X-User-Id");
+//        return ResponseEntity.ok("Welcome : "+ userId);
+//    }
+
     @GetMapping("/test")
     public ResponseEntity<String> welcome(){
-        return ResponseEntity.ok("Welcome");
+        String userId = authContextHolder.getUserIdFromCH();
+        return ResponseEntity.ok("Welcome : "+ userId);
     }
 
     @PostMapping("/createPost")
